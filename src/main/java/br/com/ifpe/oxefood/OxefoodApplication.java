c0a909d5bff7431f3bd5211a44639d3f0a21b8f6
Roberto Alencar <rroberto.alencar@gmail.com>
2022-09-14 17:33:39 -0300

    atualizando o pom e configurando o banco de dados

--- src/main/java/br/com/ifpe/oxefood/OxefoodApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApplication.java
@@ -3,11 +3,15 @@ package br.com.ifpe.oxefood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Roberto Alencar
+ *
+ */
 @SpringBootApplication
 public class OxefoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OxefoodApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(OxefoodApplication.class, args);
+    }
 
 }
