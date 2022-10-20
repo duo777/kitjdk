25c9d65c2d33cfa367a417b84373b288e8e018a9
Roberto Alencar <rroberto.alencar@gmail.com>
2022-10-20 16:15:09 -0300

    adicionando o swagger no projeto

--- src/main/java/br/com/ifpe/oxefood/servicos/produto/CategoriaProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/servicos/produto/CategoriaProdutoController.java
@@ -17,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ifpe.oxefood.modelo.produto.CategoriaProduto;
 import br.com.ifpe.oxefood.modelo.produto.CategoriaProdutoService;
 import br.com.ifpe.oxefood.util.entity.GenericController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * @author Roberto Alencar
@@ -29,6 +32,7 @@ public class CategoriaProdutoController extends GenericController {
     @Autowired
     private CategoriaProdutoService categoriaProdutoService;
 
+    @ApiOperation(value = "Serviço responsável por salvar uma categoria de produto no sistema.")
     @PostMapping
     public ResponseEntity<CategoriaProduto> save(@RequestBody @Valid CategoriaProdutoRequest request) {
 
@@ -37,12 +41,21 @@ public class CategoriaProdutoController extends GenericController {
 	return new ResponseEntity<CategoriaProduto>(categoriaProduto, HttpStatus.CREATED);
     }
     
+    @ApiOperation(value = "Serviço responsável por obter uma categoria de produto referente ao Id passado na URL.")
+    @ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Retorna a categoria de produto."),
+	    @ApiResponse(code = 401, message = "Acesso não autorizado."),
+	    @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso."),
+	    @ApiResponse(code = 404, message = "Não foi encontrado um registro para o Id informado."),
+	    @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
+    })
     @GetMapping("/{id}")
     public CategoriaProduto get(@PathVariable Long id) {
 
 	return categoriaProdutoService.findById(id);
     }
     
+    @ApiOperation(value = "Serviço responsável por obter uma lista de categorias de produto da empresa passado na URL.")
     @GetMapping("/porempresa/{chaveEmpresa}")
     public List<CategoriaProduto> consultarPorChaveEmpresa(@PathVariable String chaveEmpresa) {
 	
